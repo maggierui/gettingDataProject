a0cf356ab7b6397826c3a2c0e2e4644cedf9e9e4
Rui Hu <maggiehu.china@gmail.com>
2015-10-23 22:37:14 -0400

    Oct 23 night

--- getdata_project.R
+++ getdata_project.R
@@ -1,13 +1,13 @@
 ##download dataset zip file
 file.URL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(file.URL,destfile="./data/mobile.zip",method="auto")
+download.file(file.URL,destfile="mobile.zip",method="auto")
 ##unzip file
-unzip("./data/mobile.zip",exdir="./data")
+unzip("mobile.zip",exdir="./")
 
 ##read X_train.txt
-dat_train<-read.table("./data/UCI HAR Dataset/train/X_train.txt")
+dat_train<-read.table("UCI HAR Dataset/train/X_train.txt")
 ##read X_test.txt
-dat_test<-read.table("./data/UCI HAR Dataset/test/X_test.txt")
+dat_test<-read.table("UCI HAR Dataset/test/X_test.txt")
 
 ##merge train and test data and store it in new data frame "dat". Now dat has 561 variables with variable names of "V1, V2, ..."
 dat<-rbind(dat_train,dat_test)
@@ -32,7 +32,7 @@ dat_label[,1]<-as.character(dat_label[,1])
 
 ##use revalue in plyr package to relabel activities
 library(plyr)
-dat_label<-revalue(dat_label$Activity,c("1"="WALKING","2"="WALKING_UPSTAIRS","3"="WALKING_DOWNSTAIRS","4"="SITTING","5"="STANDING","6"="LAYING"))
+dat_label[,1]<-revalue(dat_label$Activity,c("1"="WALKING","2"="WALKING_UPSTAIRS","3"="WALKING_DOWNSTAIRS","4"="SITTING","5"="STANDING","6"="LAYING"))
 
 ## read in subject IDs for train and test data
 dat_train_subject<-read.table("./data/UCI HAR Dataset/train/subject_train.txt")
@@ -45,7 +45,26 @@ colnames(dat_subject)<-"ID"
 ##combine subject ID, activity type, and actual data
 dat<-cbind(dat_subject,dat_label,dat)
 
+##Note: there are many threads on the discussion forum about "to what level is descriptive". 
+##      I choose to follow the "all lower case" and "no special character" rules.
+##      Meanwhile, I decide not to replace every arrievated words to their full versions, for the sake of readability.
 
+##1. Get rid of all dashes and brackets (punctuations)
+colnames(dat)<-gsub ("[[:punct:]]", "", colnames(dat))
 
+##2. Change the first letter to their full words. "t" -> "time"; "f"->"frequency"
+colnames(dat)<-gsub("^[t]+","time",colnames(dat))
+colnames(dat)<-gsub("^[f]+","frequency",colnames(dat))
 
+##3. Change all capital letters to lower cases
+colnames(dat)<-tolower(colnames(dat))
+cn<-colnames(dat)
 
+##4. correct the typo of "bodybody" to "body"
+colnames(dat)<-gsub("bodybody","body",colnames(dat))
+
+## Next: calculate mean for each variable, grouped by id and activity.
+columns<-c("id","activity")
+dat %>%
+  group_by_(.dots = columns) %>%
+  summarise(timebodyaccstdx = mean(timebodyaccstdx))
